fbc5f930ae31c8a0e64397a78fe7f4f3a43bbf42
datalabb <securitasdatalab@gmail.com>
2023-05-23 16:16:41 +0300

    güncelleme

--- lokasyon_finans_codes.R
+++ lokasyon_finans_codes.R
@@ -2,6 +2,8 @@
 
 #FİNANS
 
+finans_tbl <- readRDS("C:/Users/tugce.topcu/Desktop/R/Data/finans_tbl.rds")
+
 #Birim/Proje Bazında Kar Yüzdelerinin Hesaplanması
 
 #finans_tbl <- DMRPROFIT1SUMMARY where PERIODYEAR = 2023
@@ -26,3 +28,58 @@ kar1_data <- filtered_tbl %>%
   summarize(Avg_KAR1 = mean(KAR1)) %>% 
   ungroup()
 
+
+
+
+################# GÖKHAN USTA PART #####################
+
+
+finans_tbl_kar <- finans_tbl %>%
+  
+  filter(PERIODMONTH < max(PERIODMONTH)-1) %>%
+  select(AMOUNTCREDIT, AMOUNTDEBIT, BRANCHID, BUSINESSUNIT, CUSTACCOUNT, EGC, MILLI, PERIODENDDATE, PERIODMONTH, PERIODYEAR, PROJID) %>%
+  group_by(CUSTACCOUNT, PROJID, EGC, MILLI, PERIODYEAR, PERIODMONTH, PERIODENDDATE) %>%
+  summarise(AMOUNTCREDIT2 = sum(AMOUNTCREDIT),
+            AMOUNTDEBIT2 = sum(AMOUNTDEBIT)) %>%
+  
+  mutate(Gelir = AMOUNTCREDIT2 * -1,
+         Gider = AMOUNTDEBIT2 * -1,
+         Kar1 = Gelir + Gider,
+         Kar1_Yuzde = Kar1 / Gelir)
+
+
+
+finans_tbl_kar_cum <- finans_tbl_kar %>%
+  
+  group_by(CUSTACCOUNT, PROJID) %>%
+  
+  mutate(Gelir_cum = cumsum(Gelir),
+         Gider_cum = cumsum(Gider),
+         Kar1_cum = Gelir_cum + Gider_cum,
+         Kar1_Yuzde_cum = Kar1_cum / Gelir_cum) %>%
+  ungroup()
+
+
+
+# filter(CUSTACCOUNT == "M03014")
+
+
+
+finans_tbl_kar_cum <- finans_tbl_kar_cum [!finans_tbl_kar_cum$CUSTACCOUNT == "", ]
+finans_tbl_kar_cum
+
+
+
+finans_tbl_kar_last <- finans_tbl_kar_cum %>%
+  filter(PERIODMONTH == max(PERIODMONTH)) %>%
+  select(CUSTACCOUNT, PROJID, PERIODMONTH, Gelir_cum, Gider_cum, Kar1_cum, Kar1_Yuzde_cum) %>%
+  rename(AxProjeId = CUSTACCOUNT)
+
+
+
+finans_tbl_kar_last <- finans_tbl_kar_last [!finans_tbl_kar_last$Kar1_Yuzde_cum == -Inf, ]
+finans_tbl_kar_last <- finans_tbl_kar_last [!finans_tbl_kar_last$Kar1_Yuzde_cum == Inf, ]
+
+
+
+finans_tbl_kar_last %>% view()
